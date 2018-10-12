7ef79957295a4a15c7784d83c751e23c0997ecce
shzwon <rwu5@stuy.edu>
2018-10-12 10:51:56 -0400

    should work

--- point.java
+++ point.java
@@ -1,21 +1,33 @@
-public class Point{
-  private double x,y;
+class Point {
+private double x,y;
 
-  public Point(double X, double Y){
-    x=X;
-    y=Y;
+public Point(double x, double y){
+    x=x;
+    y=y;
   }
 
-  public Point(Point p){
-    x= p.x;
-    y= p.y;
+  public Point(Point p) {
+    x = p.x;
+    y = p.y;
   }
 
-  public double getX(){
+  public double getX() {
     return x;
   }
 
-  public double getY(){
+  public double getY() {
     return y;
+  }
+
+  public double distanceTo(Point new) {
+    return Math.sqrt((new.x - x)*(new.x - x)+(new.y - y)*(new.y - y));
+  }
+
+  public static double distance(Point a, Point b) {
+    return a.distanceTo(b);
+  }
+
+  public String toString() {
+    return ("("+x+", "+y+")");
   }
 }
--- triangle.java
+++ triangle.java
@@ -13,11 +13,30 @@ public class Triangle {
 		c = new Point(x3, y3);
 	}
 
-	public double getPerm() {
+	public double getPerimeter() {
 		return a.distanceTo(b) + b.distanceTo(c) + c.distanceTo(a);
 	}
 
-	public String toString() {
-		return "Triangle: " + "A" + a + " B" + b + " C" + c;
-	}
+  public Point getVertex(int num) {
+    switch (num) {
+      case 0: return new Point(v1);
+      case 1: return new Point(v2);
+      case 2: return new Point(v3);
+    }
+    return new Point(0.0, 0.0);
+  }
+
+  public void setVertex(int num, Point newPoint) {
+    switch (num) {
+      case 0: v1 = newPoint;
+      break;
+      case 1: v2 = newPoint;
+      break;
+      case 2: v3 = newPoint;
+    }
+  }
+
+  public String toString() {
+    return "Triangle: " + "A" + a.toString() + " B" + b.toString() + " C" + c.toString();
+  }
 }
